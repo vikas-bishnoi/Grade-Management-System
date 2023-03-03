45989ab888474926dcd0b9adbc50c1af752b7878
vikaskbm <vikaskbm@gmail.com>
2023-03-03 23:19:51 +0530

    Add spring security dependency and create BCryptPasswordEncoder bean

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 
@@ -47,4 +49,8 @@ public class GradeSubmissionApplication implements CommandLineRunner {
 			courseRepository.save(course);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
