827f47df83a9d7a16ad5b46317e09eaae021524c
vikaskbm <vikaskbm@gmail.com>
2023-03-03 23:52:11 +0530

    Create register user functionality

--- grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ltp.gradesubmission.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import lombok.AllArgsConstructor;
@@ -21,11 +22,11 @@ public class SecurityConfig {
             .and()
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
+            .requestMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
+            .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
             .anyRequest().authenticated()
             .and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
-    
 }
